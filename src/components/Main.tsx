0432ebdff89281006cbb7b095536abe33d4a7f32
Diego Santos <diegofmsantos0@gmail.com>
2023-06-14 16:36:16 -0300

    add dist

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,13 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { BsArrowUpRight } from "react-icons/bs";
+import banner1 from "../assets/banner-1.png";
 import banner2 from "../assets/banner-2.png";
 import banner3 from "../assets/banner-3.png";
 import banner4 from "../assets/banner-4.png";
 import banner5 from "../assets/banner-5.png";
+import banner6 from "../assets/banner-6.png";
+import banner7 from "../assets/banner-7.png";
+
 import InputMask from 'react-input-mask';
 
 export const Main = () => {
@@ -11,7 +15,7 @@ export const Main = () => {
     return (
         <main className="">
 
-            <section className="w-screen h-screen md:h-screen bg-[url('src/assets/banner-1.png')] bg-no-repeat bg-cover bg-bottom flex">
+            <section style={{backgroundImage: `url(${banner1})`}} className="w-screen h-screen md:h-screen bg-no-repeat bg-cover bg-bottom flex">
                 <div className="w-full flex flex-col justify-center gap-20 items-center md:px-12 md:justify-around md:items-start md:p-20 lg:flex lg:flex-row lg:justify-center lg:items-start xl:justify-between xl:px-14 xl:items-center">
                     <div className="w-64 min-[375px]:w-72 md:flex md:flex-col md:items-start lg:mt-8 lg:tracking-tight lg:ml-4">
                         <h1 className="w-64 min-[375px]:w-72 min-[425px]:w-80 text-3xl text-white font-semibold md:text-5xl md:w-[450px] xl:w-[490px]  mb-4">O futuro é a razão que a gente faz planos.</h1>
@@ -81,7 +85,7 @@ export const Main = () => {
                 </div>
             </section>
 
-            <section className="w-full h-full bg-[url('src/assets/banner-6.png')] bg-no-repeat bg-cover bg-left-bottom flex justify-center items-start md:w-screen md:justify-start lg:h-screen">
+            <section style={{backgroundImage: `url(${banner6})`}} className="w-full h-full bg-no-repeat bg-cover bg-left-bottom flex justify-center items-start md:w-screen md:justify-start lg:h-screen">
                 <div className="w-72 flex flex-col items-end py-20 px-4 font-GraphikMedium md:w-[590px] md:pt-40 md:ml-28">
                     <h1 className="text-white font-semibold text-3xl md:text-[48px] md:leading-tight md:mb-4">Nós acreditamos que só uma pessoa pode ter controle sobre sua vida financeira: você.</h1>
                     <div className="text-normal flex justify-start items-center gap-2 font-bold mt-4 mr-auto text-white hover:underline">
@@ -91,7 +95,7 @@ export const Main = () => {
                 </div>
             </section>
 
-            <section className="w-full h-full bg-[url('src/assets/banner-7.png')] bg-no-repeat bg-cover bg-left-bottom flex justify-center items-start md:w-screen md:justify-start lg:h-screen">
+            <section style={{backgroundImage: `url(${banner7})`}} className="w-full h-full bg-no-repeat bg-cover bg-left-bottom flex justify-center items-start md:w-screen md:justify-start lg:h-screen">
                 <div className="w-72 flex flex-col py-20 px-4 font-GraphikMedium md:w-[590px] md:pt-40 md:ml-28">
                     <h1 className="text-white font-semibold text-3xl md:text-[48px] md:leading-tight">NuCommunity</h1>
                     <h1 className="text-white font-semibold text-3xl md:text-[48px] md:leading-tight md:mb-4">A comunidade oficial do Nubank para você tirar dúvidas e compartilhar ideias.</h1>
